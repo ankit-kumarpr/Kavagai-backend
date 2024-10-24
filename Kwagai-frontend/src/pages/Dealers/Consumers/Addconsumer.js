d778fcbb2e106aca8edc5f2c711522f71941bcf2
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2024-10-24 17:53:47 +0530

    Update Addconsumer.js

--- Kwagai-frontend/src/pages/Dealers/Consumers/Addconsumer.js
+++ Kwagai-frontend/src/pages/Dealers/Consumers/Addconsumer.js
@@ -14,7 +14,7 @@ const AddConsumer = () => {
   if(token===null || token===""){
     navigate("/")
   }
-  const BASE_url = "http://localhost:5000";
+  const BASE_url = "http://145.223.19.45:5000";
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");

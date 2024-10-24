5f7492f0a3b5b6c342beea6d95505e13212ca413
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2024-10-24 17:55:40 +0530

    Update Consumer_orders.js

--- Kwagai-frontend/src/pages/Orders/Consumer_orders.js
+++ Kwagai-frontend/src/pages/Orders/Consumer_orders.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Consumer_orders = () => {
   const token = sessionStorage.getItem("Token");
-  const BASE_url = "http://localhost:5000";
+  const BASE_url = "http://145.223.19.45:5000";
 
   const [dealers, setDealers] = useState([]);
   const [selectedDealer, setSelectedDealer] = useState("");

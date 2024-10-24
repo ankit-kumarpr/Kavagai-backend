b0abb45275453bf2c75c6a620d92bb9379683f21
ankit-kumarpr <146826335+ankit-kumarpr@users.noreply.github.com>
2024-10-24 17:47:48 +0530

    Update Login.js

--- Kwagai-frontend/src/components/Basic/Login.js
+++ Kwagai-frontend/src/components/Basic/Login.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
  function Login() {
-  const BASE_url = "http://localhost:5000";
+  const BASE_url = "http://145.223.19.45:5000";
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
